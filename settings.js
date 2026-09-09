fd0645a8714e43e39604d21a4a54ca576b4527f1
andytech580 <andytech580@gmail.com>
2026-09-09 21:33:26 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'Andy-xmd',
+  author: '‎',
+  botName: "Andy-xmd",
+  botOwner: 'Andy', // Your name
+  ownerNumber: '256701583113', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
